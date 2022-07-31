32a40f4c0d1595bc3a9a517486e736a41c527905
kundurthivenkatakishore <kundurthivenkatakishore@gmail.com>
2022-07-31 23:12:56 +0530

    updated

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,5 +5,5 @@ export const headerData = {
     title: "Full Stack Developer",
     desciption:"Myself Kundurthi Venkata Kishore,Completed B-Tech in May,2022 in the Stream of Computer Science in Rajiv Gandhi University of Knowledge Technologies",
     image: 'https://avatars.githubusercontent.com/u/47684967?s=400&u=aa2562723dd2d75a1625e9cd72da50c952b29b26&v=4',
-    resumePdf: <a href='https://docs.google.com/document/d/1e4TQhT_FwO2cs2CqTcmF-rgqiMpld7HYNw6HPYCMi5g/edit?usp=sharing'></a>
+    resumePdf: <a href='https://drive.google.com/file/d/1zir4as5sHN-PgEI9LYM_jIE8sz3N3PB3/view?usp=sharing'></a>
 }
